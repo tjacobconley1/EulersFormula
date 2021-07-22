0f1bde929125f521848caf71f0f818fe044056fc
Tyler Conley <tjacobconley1@gmail.com>
2021-07-22 12:43:50 -0400

    Create Euler.py
    "euler....         euler.....                                   euler......                                                                          euler............"

---
+++ Euler.py
@@ -0,0 +1,56 @@
+# EULER's FORMULA
+# Author: Tyler Conley
+
+import numpy as np
+import matplotlib
+
+
+
+
+#==============================================================
+# Formula:
+#            e^ix = cos(x) + i*sin(x)
+#
+#    where 'e' = 2.718...  (the base of the natural logarithm)
+#          'x' = to degrees of rotation (in pi or radians?)
+#      and 'i' = an imaginary number (sqrt( a negative number )
+#
+# Identity:
+#              e^(i(pi) + 1) = 0
+#
+# as a convex polyhedra written:
+#
+#            F + V = E + 2
+#
+#    where 'F' is the number of faces,
+#          'V' is the number of verticies
+#      and 'E' is the number of edges
+#
+#
+#==========================================================================
+
+
+
+
+
+
+
+
+# when x = pi or 2 pi Euler's equation yields two simultaneous expressions
+#
+#          e^(i(pi)) = -1        and         e^(2i(pi)) = 1
+#
+#
+#==========================================================================
+
+
+
+
+
+
+
+
+
+
+# can be used for topology=================================================
+#==========================================================================
